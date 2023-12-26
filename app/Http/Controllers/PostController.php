c6adfe7ee66a33fce58f4066f8954a02ca24d018
Marco Julian <marcos_019@hotmail.com>
2023-12-25 19:21:38 -0600

    115 Using Cache with Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,13 +21,13 @@ class PostController extends Controller
      */
     public function index()
     {
-        // $posts = Cache::remember('posts', 60, function(){
-        //     return Post::with('category')->paginate(4);
-        // });
-
-        $posts = Cache::rememberForever('posts', function(){
+        $posts = Cache::remember('posts-page-'.request('page', 1), 60*3, function(){
             return Post::with('category')->paginate(4);
-        });        
+        });
+
+        // $posts = Cache::rememberForever('posts', function(){
+        //     return Post::with('category')->paginate(4);
+        // });        
 
         return view('index', compact('posts'));
     }
