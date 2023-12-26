565aef926e668bab9667148693f042db33bc83b8
Marco Julian <marcos_019@hotmail.com>
2023-12-25 19:12:19 -0600

    114 Removing Data From Cache

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
 
 /*
@@ -100,4 +101,8 @@ Route::get('flash-session', function(Request $request)
     session()->flash('status', 'true');
 
     return redirect('get-session');
+});
+
+Route::get('forget-cache', function(){
+   Cache::forget('posts');
 });
